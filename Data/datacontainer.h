6eca62e335773e3ac299a6eb43865de0daf8dec6
balogunb <balogunb@lafayette.edu>
2019-04-02 01:00:21 -0400

    Basic data class methods and variables

--- Data/datacontainer.h
+++ Data/datacontainer.h
@@ -1,11 +1,31 @@
 #ifndef DATACONTAINER_H
 #define DATACONTAINER_H
+#include <vector>
+#include <string>
 
+
+using namespace std;
+
+
+struct data{
+    string hiveId;
+    string date;
+    string time;
+    int  gate;//might be needed
+    int type;//entry or exit with entry being 0 and exit being 1
+
+};
 
 class DataContainer
 {
 public:
     DataContainer();
+    void addData(data d);
+
+private:
+
+    vector<data> recent;
+
 };
 
 #endif // DATACONTAINER_H
