c94b3e75847a16643d9c9b567ce2741892e69820
whitejon <whitejon@lafayette.edu>
2019-04-30 11:35:42 -0400

    New Screens

--- LoginGUI/createmodel.h
+++ LoginGUI/createmodel.h
@@ -2,6 +2,10 @@
 #define CREATEMODEL_H
 
 #include <QDialog>
+#include "generatedmodel.h"
+#include <string>
+#include <iostream>
+using namespace std;
 
 namespace Ui {
 class CreateModel;
@@ -18,8 +22,23 @@ public:
 private slots:
     void on_pushButton_clicked();
 
+
+
+    void on_lineEdit_textChanged(const QString &arg1);
+
+    void on_lineEdit_2_textChanged(const QString &arg1);
+
+    void on_lineEdit_3_textChanged(const QString &arg1);
+
 private:
     Ui::CreateModel *ui;
+    GeneratedModel *genModel;
+
+    string time;
+    string duration;
+    string date;
+
+
 };
 
 #endif // CREATEMODEL_H
